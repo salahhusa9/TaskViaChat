93afe5750446bb869af7ec605a3ab3881e97ef42
salahhusa9 <salahhusa9@gmail.com>
2024-03-24 11:00:57 +0000

    wip job

---
+++ app/Jobs/ProcessWhatsappWebhookJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Jobs\ProcessWebhookJob as SpatieProcessWebhookJob;
+
+class ProcessWhatsappWebhookJob extends SpatieProcessWebhookJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
